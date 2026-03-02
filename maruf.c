a213707724d6cae382a007aa9f11bc5c4ec1d051
Sayed Maruf <sayedmarufprof@gmail.com>
2026-03-02 12:39:47 -0500

    hellooo

---
+++ maruf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"add.h"
+int main (){
+        int num =49;
+        int result =0;
+        for (int i =1 ; i<100;i++)
+        {
+        if (i*i== num)
+        {
+        result= i;
+        }
+        else result =0;
+        }
+printf("%d",result);
+return 0;}
+
